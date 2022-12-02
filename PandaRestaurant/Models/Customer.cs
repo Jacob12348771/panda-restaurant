d38f37862c5419802a6d4342491da1ab87a3a6b2
ClaudiaDanciu <contact@claudiadanciu.com>
2022-12-02 14:08:06 +0000

    Add Customer Model

---
+++ PandaRestaurant/Models/Customer.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PandaRestaurant.Models
+{
+    public class Customer
+    {
+        [Range(0, 6)]
+        public int CustomerId { get; set; }
+        [StringLength(50, MinimumLength = 3)]
+        public string customerName { get; set; }
+    }
+}
