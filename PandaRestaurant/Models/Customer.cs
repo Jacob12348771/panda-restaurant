f56fba575d2ab920f0566f1086bd3ec27811a23e
ClaudiaDanciu <contact@claudiadanciu.com>
2022-12-02 14:59:22 +0000

    Update customer ID

--- PandaRestaurant/Models/Customer.cs
+++ PandaRestaurant/Models/Customer.cs
@@ -5,7 +5,7 @@ namespace PandaRestaurant.Models
     public class Customer
     {
         [Range(0, 6)]
-        public int CustomerId { get; set; }
+        public int CustomerID { get; set; }
         [StringLength(50, MinimumLength = 3)]
         public string customerName { get; set; }
     }
