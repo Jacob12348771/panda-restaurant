ecaab00296c0269d353beef55cd4a8eb790f4e0f
Jacob <jsmith@synergytaste.com>
2022-12-02 15:00:52 +0000

    Added annotations + changed ID for consistency.

--- PandaRestaurant/Models/Table.cs
+++ PandaRestaurant/Models/Table.cs
@@ -1,9 +1,16 @@
-﻿namespace PandaRestaurant.Models
+﻿using System.ComponentModel.DataAnnotations;
+using System.Xml.Linq;
+
+namespace PandaRestaurant.Models
 {
     public class Table
     {
-        public int TableId { get; set; }
+        [Display(Name = "Table Number")]
+        public int TableID { get; set; }
+
+        [Display(Name = "Table Availablity")]
         public bool TableOccupied { get; set; }
+
         public ICollection<Order> Orders { get; set; }
         public Staff Staff { get; set; }
 
