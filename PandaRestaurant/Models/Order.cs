0be8eec6eacdbafd0115b1960f5b870d42bf160b
Daraoui <zakaria.daraoui@sky.uk>
2022-12-02 14:32:28 +0000

    Created initial class

---
+++ PandaRestaurant/Models/Order.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata.Internal;
+
+namespace PandaRestaurant.Models
+{
+    public class Order
+    {
+
+        public enum OrderStatus
+        {
+            Created, Preparing, Ready, Paid
+        }
+
+        public int Order_id { get; set; }
+        public OrderStatus Order_status { get; set; }
+        public DateTime Order_datetime { get; set; }
+        public int Table_id { get; set; }
+        public int Customer_id { get; set;}
+
+    }
+}
