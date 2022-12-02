9901ae7fed8ddc439d342407b5e6732f6634b5c7
Jacob <jsmith@synergytaste.com>
2022-12-02 14:07:54 +0000

    MenuItem model created.

---
+++ PandaRestaurant/Models/MenuItem.cs
@@ -0,0 +1,11 @@
+﻿namespace PandaRestaurant.Models
+{
+    public class MenuItem
+    {
+        public int MenuItemId { get; set; }
+        public string MenuItemName { get; set; }
+        public string MenuItemPrepTime { get; set; }
+        public float MenuItemPrice { get; set; }
+        public ICollection<Order> Orders { get; set; }
+    }
+}
