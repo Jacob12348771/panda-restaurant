030ce7b8feee66ccee9949b14ec1abe6e924e999
Theo Sylva <22200390@bucks.ac.uk>
2022-12-02 14:17:15 +0000

    Staff model created

---
+++ PandaRestaurant/Models/Staff.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PandaRestaurant.Models
+{
+    public class Staff
+    {
+        public int ID { get; set; }
+        [Required]
+        [StringLength(100, ErrorMessage = "You name cannot exceed 100 characters")]
+        public string staffName { get; set; }
+        [Required]
+        public string staffPosition { get; set; }
+    }
+}
