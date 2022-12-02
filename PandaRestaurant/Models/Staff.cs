39bfc171ca4a921983cda3e76d194f13077a59ef
Theo Sylva <22200390@bucks.ac.uk>
2022-12-02 15:20:14 +0000

    Fixed CamelCasing

--- PandaRestaurant/Models/Staff.cs
+++ PandaRestaurant/Models/Staff.cs
@@ -4,11 +4,11 @@ namespace PandaRestaurant.Models
 {
     public class Staff
     {
-        public int staffID { get; set; }
+        public int StaffID { get; set; }
         [Required]
         [StringLength(100, ErrorMessage = "You name cannot exceed 100 characters")]
-        public string staffName { get; set; }
+        public string StaffName { get; set; }
         [Required]
-        public string staffPosition { get; set; }
+        public string StaffPosition { get; set; }
     }
 }
