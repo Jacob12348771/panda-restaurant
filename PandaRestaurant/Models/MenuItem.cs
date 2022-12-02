b709dcc59598933b4caac260a4d79f2df9ab6c08
Jacob <jsmith@synergytaste.com>
2022-12-02 14:42:56 +0000

    MenuItem annotations added.

--- PandaRestaurant/Models/MenuItem.cs
+++ PandaRestaurant/Models/MenuItem.cs
@@ -1,10 +1,20 @@
-﻿namespace PandaRestaurant.Models
+﻿using System.ComponentModel.DataAnnotations;
+using System.Xml.Linq;
+
+namespace PandaRestaurant.Models
 {
     public class MenuItem
     {
         public int MenuItemId { get; set; }
+        [Required]
+        [Display(Name = "Menu Item Name")]
+        [StringLength(50, MinimumLength = 3)]
         public string MenuItemName { get; set; }
-        public string MenuItemPrepTime { get; set; }
+        [Display(Name = "Preparation Time")]
+        public string? MenuItemPrepTime { get; set; }
+        [DataType(DataType.Currency)]
+        [DisplayFormat(DataFormatString = "{0:C0}")]
+        [Display(Name = "Dish Price")]
         public float MenuItemPrice { get; set; }
         public ICollection<Order> Orders { get; set; }
     }
