d8b7bad53ead0d900a07f575a6c2a3cc75f780ab
Daraoui <zakaria.daraoui@sky.uk>
2022-12-23 13:29:00 +0000

    updated page for menu items@

--- PandaRestaurant/Models/MenuItem.cs
+++ PandaRestaurant/Models/MenuItem.cs
@@ -5,17 +5,17 @@ namespace PandaRestaurant.Models
 {
     public class MenuItem
     {
-        [Display(Name = "Reference Number")]
+        [Display(Name = "Product ID")]
         public int MenuItemID { get; set; }
         [Required]
-        [Display(Name = "Menu Item Name")]
+        [Display(Name = "Product Name")]
         [StringLength(50, MinimumLength = 3)]
         public string MenuItemName { get; set; }
         [Display(Name = "Preparation Time (Minutes)")]
         public int? MenuItemPrepTime { get; set; }
         [DataType(DataType.Currency)]
         //[DisplayFormat(DataFormatString = "{0:C0}")]
-        [Display(Name = "Dish Price")]
+        [Display(Name = "Price")]
         public double MenuItemPrice { get; set; }
         public ICollection<Order> Orders { get; set; }
     }
