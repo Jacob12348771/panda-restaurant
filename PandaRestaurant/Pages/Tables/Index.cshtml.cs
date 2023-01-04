ad8e3814f91f2fdc40a4dec28168ce956852552b
Jacob <jsmith@synergytaste.com>
2023-01-04 11:58:21 +0000

    Added sorting for tables.

--- PandaRestaurant/Pages/Tables/Index.cshtml.cs
+++ PandaRestaurant/Pages/Tables/Index.cshtml.cs
@@ -19,16 +19,38 @@ namespace PandaRestaurant.Pages.Tables
             _context = context;
         }
 
+        public string TableNumberSort { get; set; }
+        public string OccupiedSort { get; set; }
+
         public IList<Table> Table { get;set; } = default!;
 
-        public async Task OnGetAsync()
+        public async Task OnGetAsync(string sortOrder)
         {
-            if (_context.Table != null)
+            TableNumberSort = String.IsNullOrEmpty(sortOrder) ? "TableID_desc" : "";
+            OccupiedSort = sortOrder == "TableOccupied" ? "TableOccupied_desc" : "TableOccupied";
+
+            IQueryable<Table> tablesIQ = from s in _context.Table
+                                             select s;
+
+            switch (sortOrder)
             {
-                Table = await _context.Table
+                case "TableID_desc":
+                    tablesIQ = tablesIQ.OrderByDescending(t => t.TableID);
+                    break;
+                case "TableOccupied":
+                    tablesIQ = tablesIQ.OrderBy(t => t.TableOccupied);
+                    break;
+                case "TableOccupied_desc":
+                    tablesIQ = tablesIQ.OrderByDescending(t => t.TableOccupied);
+                    break;
+                default:
+                    tablesIQ = tablesIQ.OrderBy(t => t.TableID);
+                    break;
+            }
+
+                Table = await tablesIQ
                     .Include(o => o.Orders)
                     .ToListAsync();
-            }
         }
     }
 }
