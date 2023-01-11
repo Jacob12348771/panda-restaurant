0a1db1fd2acb4e45f660a9de16d0b114088caba5
Jacob <jsmith@synergytaste.com>
2023-01-11 10:41:47 +0000

    Added sorting for ID and date ordered.

--- PandaRestaurant/Pages/Orders/Index.cshtml.cs
+++ PandaRestaurant/Pages/Orders/Index.cshtml.cs
@@ -19,16 +19,38 @@ namespace PandaRestaurant.Pages.Orders
             _context = context;
         }
 
+        public string OrderNumberSort { get; set; }
+        public string OrderDateSort { get; set; }
+
         public IList<Order> Order { get;set; } = default!;
 
-        public async Task OnGetAsync()
+        public async Task OnGetAsync(string sortOrder)
         {
-            if (_context.Order != null)
+            OrderNumberSort = String.IsNullOrEmpty(sortOrder) ? "OrderID_desc" : "";
+            OrderDateSort = sortOrder == "OrderDateTime" ? "OrderDateTime_desc" : "OrderDateTime";
+
+            IQueryable<Order> ordersIQ = from o in _context.Order
+                                         select o;
+
+            switch(sortOrder)
             {
-                Order = await _context.Order
-                .Include(o => o.Customer)
-                .Include(o => o.Table).ToListAsync();
+                case "OrderID_desc":
+                    ordersIQ = ordersIQ.OrderByDescending(o => o.OrderID);
+                    break;
+                case "OrderDateTime":
+                    ordersIQ = ordersIQ.OrderBy(o => o.OrderDatetime).Take(10);
+                    break;
+                case "OrderDateTime_desc":
+                    ordersIQ = ordersIQ.OrderByDescending(o => o.OrderDatetime).Take(10);
+                    break;
+                default:
+                    ordersIQ = ordersIQ.OrderBy(o => o.OrderID);
+                    break;
             }
+                Order = await ordersIQ
+                .Include(o => o.Customer)
+                .Include(o => o.Table)
+                .ToListAsync();
         }
     }
 }
