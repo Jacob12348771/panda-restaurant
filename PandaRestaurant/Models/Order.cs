d8a8da5e434af787a34aa8674c08c858afc8bc1e
Jacob <jsmith@synergytaste.com>
2023-01-13 11:49:30 +0000

    Cleaning up models.

--- PandaRestaurant/Models/Order.cs
+++ PandaRestaurant/Models/Order.cs
@@ -7,27 +7,35 @@ namespace PandaRestaurant.Models
 {
     public class Order
     {
-
         public enum OrderStatusEnum
         {
             Created, Preparing, Ready, Paid
         }
 
         [Display(Name = "Order ID")]
+        [Range(0, 6)]
         public int OrderID { get; set; }
+
+        [Required]
         [Display(Name = "Order Status")]
         public OrderStatusEnum OrderStatus { get; set; }
+
+        [Required]
         [Display(Name = "Date & Time Ordered")]
         public DateTime OrderDatetime { get; set; }
+
         [ForeignKey("Table")]
         [Display(Name = "Table ID")]
         public int TableID { get; set; }
+
         [ForeignKey("Customer")]
         [Display(Name = "Customer ID")]
         public int CustomerID { get; set;}
-        public ICollection<MenuItem> MenuItem { get; set; }
-        public Table Table { get; set; }
-        public Customer Customer { get; set; }
 
+        public ICollection<MenuItem> MenuItem { get; set; }
+
+        public Table Table { get; set; }
+
+        public Customer Customer { get; set; }
     }
 }
